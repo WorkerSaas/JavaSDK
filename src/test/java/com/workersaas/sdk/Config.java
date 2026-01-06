ff04a22271b4818937e17474bb9bffeead2a5d66
Hamm <majhamm@qq.com>
2026-01-06 17:36:16 +0800

    fix(bugs): 修复了一些已知的BUG

--- src/test/java/com/workersaas/sdk/Config.java
+++ src/test/java/com/workersaas/sdk/Config.java
@@ -6,13 +6,8 @@ import com.workersaas.sdk.common.base.WorkerSaasConstant;
 import com.workersaas.sdk.common.enums.WorkerSaasArithmetic;
 
 public class Config {
-    // 生产辽宁理账狮
-//    public static final String APP_KEY = "ANp7Mhm4XzpSv8wH5JHgApAN0g8vWBcL";
-//    public static final String APP_SECRET = "ol87UF7IC7a7xvDqYZ2yW449kYiMLnfJYVJubVnPd50=";
-
-    // 广州璇玑
-    public static final String APP_KEY = "mDRJnMRYUd1ZHshLKLAIrGrvrhZy13Ik";
-    public static final String APP_SECRET = "xbuaXZFsNthQkqMBcUZ8DvY+EkcwW3dlLJNt4Va0sts=";
+    public static final String APP_KEY = "";
+    public static final String APP_SECRET = "";
 
 
     public static final String GATEWAY = WorkerSaasConstant.GATEWAY_PRODUCTION;
